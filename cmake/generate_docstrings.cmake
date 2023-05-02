94b255c1ec39f78e5eeba6ebf1dda73f8eec11c7
Jérôme Guzzi <jerome@idsia.ch>
2023-05-02 14:35:51 +0200

    Corrected lists

--- cmake/generate_docstrings.cmake
+++ cmake/generate_docstrings.cmake
@@ -1,10 +1,10 @@
-function(generate_docstrings target python_src library include deps)
+function(generate_docstrings target python_src library include)
   add_custom_target(
       docstrings ALL
       COMMAND 
          ${Python3_EXECUTABLE} 
           ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/generate_docstrings.py 
-          ${python_src} ${library} ${include} ${deps} 
+          ${python_src} ${library} ${include} ${ARGN} 
       COMMENT "Generate docstrings" 
       OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/docstrings.h
   )
